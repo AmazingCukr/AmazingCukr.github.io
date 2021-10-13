e22c1149ffc4b3bd8c0425f2e21ae7d92dbff0a2
AmazingCukr <61467194+AmazingCukr@users.noreply.github.com>
2021-10-13 19:01:47 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -10,8 +10,7 @@ slideshowImages[currentImageCounter].style.opacity = "1";
 setInterval(nextImage, nextImageDelay);
 
 function nextImage(){
-    //slideshowImages[currentImageCounter].style.opacity = "0";
-    
+        
     slideshowImages[currentImageCounter].style.zIndex = "-2";
     const tempCounter = currentImageCounter;
 
@@ -25,4 +24,29 @@ function nextImage(){
     slideshowImages[currentImageCounter].style.opacity = "1";
     slideshowImages[currentImageCounter].style.zIndex = "-1";
 }
-//
+
+
+const modal = document.querySelector('.modal')
+const previews = document.querySelectorAll('.photos')
+const original = document.querySelector('.full-img')
+
+
+
+previews.forEach(preview => {
+    preview.addEventListener('click', () => {
+        modal.classList.add('open');
+        original.classList.add('open');
+
+        const originalSrc = preview.getAttribute("data-original");
+        //original.src = originalSrc;
+        console.log(originalSrc)
+        
+    });
+});
+
+modal.addEventListener('click', (e) => {
+    if(e.target.classList.contains('modal')){
+        modal.classList.remove('open')
+        original.classList.remove('open');
+    }
+});
