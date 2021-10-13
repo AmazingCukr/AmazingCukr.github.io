12688e399fac886401e0508883e868478fa501bb
AmazingCukr <61467194+AmazingCukr@users.noreply.github.com>
2021-10-13 22:00:17 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ previews.forEach(preview => {
 
         const originalSrc = preview.getAttribute("data-original");
         //original.src = originalSrc;
-        console.log(originalSrc)
+        
         
     });
 });
@@ -50,3 +50,30 @@ modal.addEventListener('click', (e) => {
         original.classList.remove('open');
     }
 });
+
+const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+    const navLinks = document.querySelectorAll('.nav-link li');
+
+    burger.addEventListener('click', ()=>{
+        nav.classList.toggle('nav-active');
+    
+        navLinks.forEach((link, index) =>{
+        navLinks.forEach((link, index) => {
+            if(link.style.animation){
+                link.style.animation='';
+            } else {
+                link.style.animation =`navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+            });
+        });
+
+        burger.classList.toggle('toggle');
+
+    });
+
+
+}
+
+navSlide();
